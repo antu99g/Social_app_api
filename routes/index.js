cc47d08f2063a4c54fe41bb8b3d366c63e463c5e
Antu <antu99gayen@gmail.com>
2023-06-25 21:37:12 +0530

    allow fetching all posts without authentication

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-console.log('router loaded');
+console.log("router loaded");
 
 const auth = require("../middlewares/auth");
 
-const homeController = require('../controllers/home_controller');
-
+const homeController = require("../controllers/home_controller");
 
 // Route to get all friends
 router.get("/friends", auth.verifyUser, homeController.fetchAllFriends);
 
 // Route to get all posts
-router.get("/posts", auth.verifyUser, homeController.fetchAllPosts);
+router.get("/posts", homeController.fetchAllPosts);
 
 // Route to get all users
 router.get("/users", homeController.fetchAllUsers);
 
-
-router.use('/user', require('./user'));
-router.use('/post', require('./post'));
-router.use('/comment', require('./comment'));
+router.use("/user", require("./user"));
+router.use("/post", require("./post"));
+router.use("/comment", require("./comment"));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const auth = require("../middlewares/auth");
 
-const userController = require('../controllers/user_controller');
+const userController = require("../controllers/user_controller");
 
-const handleImg = require('../middlewares/imageUpload');
-
+const handleImg = require("../middlewares/imageUpload");
 
 // Route to register new user
 router.post("/signup", userController.signup);
@@ -18,14 +17,18 @@ router.post("/login", userController.login);
 router.get("/:id/details", userController.userDetails);
 
 // Route to get all posts of a user
-router.get("/:id/posts", auth.verifyUser, userController.fetchPosts);
+router.get("/:id/posts", userController.fetchPosts);
 
 // Route to edit user details
 router.post("/edit", auth.verifyUser, userController.updateUserDetails);
 
 // Route to edit profile image
-router.post("/edit/avatar", handleImg.uploadedAvatar, auth.verifyUser, userController.updateUserAvatar);
-
+router.post(
+  "/edit/avatar",
+  handleImg.uploadedAvatar,
+  auth.verifyUser,
+  userController.updateUserAvatar
+);
 
 router.use("/friend", require("./friend"));
 
